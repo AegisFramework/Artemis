e5e629a2a4195a36ed3bc63372a17cb3a4992ad7
Diego Islas Ocampo <hyuchia@gmail.com>
2017-06-10 00:39:12 -0500

    Make AJAX request with promises

--- dist/artemis.js
+++ dist/artemis.js
@@ -411,65 +411,66 @@ function $_ready(callback){
 
 class Request {
 
-	static get(url, data, events, responseType = ""){
-		var request = new XMLHttpRequest();
-		request.open('GET', url, true);
-		request.responseType = responseType;
+	static get(url, data, responseType = ""){
+		return new Promise(function (resolve, reject) {
+			var encodedData = [];
+			for(var value in data){
+				encodedData.push(encodeURIComponent(value) + "=" + encodeURIComponent(data[value]));
+			}
+			var request = new XMLHttpRequest();
+			request.open('GET', url + "?" + encodedData.join("&"), true);
+			request.responseType = responseType;
 
-		if(typeof events.onload === "function"){
 			request.onload = function(){
-				events.onload(request);
+				resolve(request.response);
 			}
-		}
 
-		if(typeof events.error === "function"){
 			request.error = function(){
-				events.error(request);
+				reject(request);
 			}
-		}
 
-		request.send(data);
+			request.send();
+		});
 	}
 
-	static post(url, data, events, responseType = "", contentType = 'application/x-www-form-urlencoded'){
-		var request = new XMLHttpRequest();
-		request.open('POST', url, true);
-		request.responseType = responseType;
-		if(typeof events.onload === "function"){
+	static post(url, data, responseType = "", contentType = 'application/x-www-form-urlencoded'){
+		return new Promise(function (resolve, reject) {
+			var encodedData = [];
+			for(var value in data){
+				encodedData.push(encodeURIComponent(value) + "=" + encodeURIComponent(data[value]));
+			}
+			var request = new XMLHttpRequest();
+			request.open('POST', url, true);
+			request.responseType = responseType;
 			request.onload = function(){
-				events.onload(request);
+				resolve(request.response);
 			}
-		}
 
-		if(typeof events.error === "function"){
 			request.error = function(){
-				events.error(request);
+				reject(request);
 			}
-		}
 
-		request.setRequestHeader('Content-Type', `${contentType}; charset=UTF-8`);
-		request.send(data);
+			request.setRequestHeader('Content-Type', `${contentType}; charset=UTF-8`);
+			request.send(encodedData.join("&"));
+		});
 	}
 
-	static json(url, events){
-		var request = new XMLHttpRequest();
+	static json(url){
+		return new Promise(function (resolve, reject) {
+			var request = new XMLHttpRequest();
 
-		request.responseType = "json";
-
-		if(typeof events.onload === "function"){
+			request.responseType = "json";
 			request.onload = function(){
-				events.onload(request);
+				resolve(request.response);
 			}
-		}
 
-		if(typeof events.error === "function"){
 			request.error = function(){
-				events.error(request);
+				reject(request);
 			}
-		}
 
-		request.open('GET', url, true);
-		request.send();
+			request.open('GET', url, true);
+			request.send();
+		});
 	}
 
 }
