5f879c2899162fb28aa162d91951d194e753820e
Diego Islas Ocampo <dev@hyuchia.com>
2018-06-06 13:48:47 -0500

    Simplify index exports

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-import { Aegis, $_, $_ready } from './lib/Artemis';
-import { Form } from './lib/Form';
-import { Request } from './lib/Request';
-import { Screen } from './lib/Screen';
-import { Storage } from './lib/Storage';
-import { Text } from './lib/Text';
-
-
-export { Aegis, $_, $_ready };
-export { Form };
-export { Request };
-export { Screen };
-export { Storage };
-export { Text };
+export * from './lib/Artemis';
+export * from './lib/Form';
+export * from './lib/Request';
+export * from './lib/Screen';
+export * from './lib/Storage';
+export * from './lib/Text';
