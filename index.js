e9ed8f3cc059b1d5478a5b587d4c457e20c3690e
Diego Islas Ocampo <dev@hyuchia.com>
2018-06-06 00:35:04 -0500

    Change project structure to a modular one

---
+++ index.js
@@ -0,0 +1,14 @@
+import { Aegis, $_, $_ready } from './lib/Artemis';
+import { Form } from './lib/Form';
+import { Request } from './lib/Request';
+import { Screen } from './lib/Screen';
+import { Storage } from './lib/Storage';
+import { Text } from './lib/Text';
+
+
+export { Aegis, $_, $_ready };
+export { Form };
+export { Request };
+export { Screen };
+export { Storage };
+export { Text };
